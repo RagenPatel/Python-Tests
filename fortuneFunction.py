e4792aa321a430accbafab09278f99d4a7a8f99d
Ragen <ragenpatel@live.com>
2017-05-01 20:55:51 -0400

    Update fortuneFunction.py

--- fortuneFunction.py
+++ fortuneFunction.py
@@ -3,9 +3,9 @@ def getAnswer(ansNum):
     if ansNum == 1:
         return "Maybe \n RandNum = " + str(ansNum)
     elif ansNum >1 and ansNum<5:
-        return "Holy Shiet \n RandNum = " + str(ansNum)
+        return "wow \n RandNum = " + str(ansNum)
     elif ansNum>=5 and ansNum<=8:
-        return "dayum Dawg \n RandNum = " + str(ansNum)
+        return "dam \n RandNum = " + str(ansNum)
     elif ansNum>8 and ansNum<=10:
         return "doubtful \n RandNum = " + str(ansNum)
 
